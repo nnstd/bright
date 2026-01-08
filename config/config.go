8488d76b61ea25325758b841b486f79d8bce2050
Nikita Ganzikov <nganzikov@gmail.com>
2026-01-08 22:03:06 +0300

    fix: update RaftDir configuration to default to DataPath/raft if not set

--- config/config.go
+++ config/config.go
@@ -16,7 +16,7 @@ type Config struct {
 	// Raft configuration
 	RaftEnabled   bool   `env:"RAFT_ENABLED" envDefault:"false"`
 	RaftNodeID    string `env:"RAFT_NODE_ID"`
-	RaftDir       string `env:"RAFT_DIR" envDefault:"./data/raft"`
+	RaftDir       string `env:"RAFT_DIR"`
 	RaftBind      string `env:"RAFT_BIND" envDefault:"0.0.0.0:7000"`
 	RaftBootstrap bool   `env:"RAFT_BOOTSTRAP" envDefault:"false"`
 	RaftPeers     string `env:"RAFT_PEERS"` // Comma-separated peer addresses
@@ -28,6 +28,12 @@ func Load() (*Config, error) {
 	if err := env.Parse(cfg); err != nil {
 		return nil, err
 	}
+
+	// Set RaftDir to DataPath/raft if not explicitly configured
+	if cfg.RaftDir == "" {
+		cfg.RaftDir = cfg.DataPath + "/raft"
+	}
+
 	return cfg, nil
 }
 
